25546ea055d97e7ffdb2375e6fdbadb671a78620
Muhammad Waqas (Ui/Ux Designer) <m.waqas@avanza.pk>
2024-07-31 20:57:12 +0500

    navigate issue resolve 4

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import {
   Routes,
   useLocation,
-  Route
+  Route,
+  Navigate
 } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 
